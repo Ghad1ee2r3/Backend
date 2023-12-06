72cc966ea2e5156b6e15205d64f8cba10f849ce8
Ghad1ee2r3 <apps02977@gmail.com>
2023-12-06 13:55:49 +0300

    test changeing in API

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ app.get('/',(req,res)=>{
     res.send('start server')
 })
 
+
+app.get('/products',(req,res)=>{
+    res.send({
+        products:[
+            {id:1, title:'apple', price:100},
+            {id:2, title:'mac', price:200},
+
+        ]
+    })
+})
+
 app.listen(3002,()=>{
     console.log(`server running at 3002`);
 })
